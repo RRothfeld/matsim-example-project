0e09798c104a3dbb17620ab6c35764f07dfe6084
RRothfeld <raoul.rothfeld+github@gmail.com>
2020-07-19 16:20:17 +0200

    Update GenerateRandomDemandSolution.java

--- src/main/java/org/matsim/project/lecture6/GenerateRandomDemandSolution.java
+++ src/main/java/org/matsim/project/lecture6/GenerateRandomDemandSolution.java
@@ -25,17 +25,17 @@ import java.util.Random;
 
 public class GenerateRandomDemandSolution {
 	// Specify all input files
-	private static final String COUNTIESFILE = "sample data/lkr_ex.shp"; // Polygon shapefile for demand generation
-	private static final String COUNTIESID = "SCH";
-	private static final String PLANSFILEOUTPUT = "sample data/plans.xml"; // The output file of demand generation
+	private static final String COUNTIESFILE = "NUTS3.shp"; // Polygon shapefile for demand generation
+	private static final String COUNTIESID = "id";
+	private static final String PLANSFILEOUTPUT = "plans.xml"; // The output file of demand generation
 
 	// Due to the huge number of commuters, it's preferable to decrease the size of simulating agents (sampling).
-	private static double SCALEFACTOR = 0.01;
+	private static double SCALEFACTOR = 1;
 
 	// Define the coordinate transformation function
 	// If an error occurs here with geotools, you need to run this using Java 8
 	private final CoordinateTransformation transformation = TransformationFactory
-			.getCoordinateTransformation("EPSG:31468", TransformationFactory.DHDN_GK4);
+			.getCoordinateTransformation("EPSG:31467", TransformationFactory.DHDN_GK4);
 
 	// Define objects and parameters
 	private Scenario scenario;
@@ -56,29 +56,10 @@ public class GenerateRandomDemandSolution {
 		this.shapeMap = readShapeFile(COUNTIESFILE, COUNTIESID);
 
 		// write a new method to create OD relations
-		createOD(2005, 0.6, "09162", "09761", "Munich City-Augsburg City");
-		createOD(5491, 0.4, "09162", "09174", "Munich City-Dachau");
-		createOD(5761, 0.4, "09162", "09175", "Munich City-Ebersberg");
-		createOD(3364, 0.5, "09162", "09177", "Munich City-Erding");
-		createOD(9918, 0.5, "09162", "09178", "Munich City-Freising");
-		createOD(7065, 0.6, "09162", "09179", "Munich City-Fuerstenfeldbruck");
-		createOD(1776, 0.8, "09162", "09161", "Munich City-Ingolstadt City");
-		createOD(78001, 0.3, "09162", "09184", "Munich City-Munich County");
-		createOD(2169, 0.7, "09162", "09182", "Munich City-Miesbach");
-		createOD(8137, 0.5, "09162", "09188", "Munich City-Starnberg");
+		createOD(100, "car", "46", "162", "carUser");
+		createOD(100, "pt", "46", "162", "ptUser");
+		createOD(100, "uam", "46", "162", "hyperloopUser");
 
-		createOD(9170, 0.6, "09761", "09162", "Augsburg City-Munich City");
-		createOD(27831, 0.4, "09174", "09162", "Dachau-Munich City");
-		createOD(21257, 0.4, "09175", "09162", "Ebersberg-Munich City");
-		createOD(12767, 0.5, "09177", "09162", "Erding-Munich City");
-		createOD(18339, 0.5, "09178", "09162", "Freising-Munich City");
-		createOD(35780, 0.6, "09179", "09162", "Fuerstenfeldbruck-Munich City");
-		createOD(64479, 0.3, "09184", "09162", "Munich County-Munich City");
-		createOD(15669, 0.5, "09188", "09162", "Starnberg-Munich City");
-		createOD(8065, 0.6, "09186", "09162", "Pfaffenhofen-Munich City");
-		createOD(8360, 0.6, "09163", "09162", "Rosenheim-Munich City");
-
-		createOD(466885, 0.6, "09162", "09162", "Munich City-Munich City");
 
 		// Write the population file to specified folder
 		PopulationWriter pw = new PopulationWriter(scenario.getPopulation(), scenario.getNetwork());
@@ -86,10 +67,9 @@ public class GenerateRandomDemandSolution {
 	}
 
 	// Create od relations for each counties
-	private void createOD(int pop, double shareOfcar, String origion, String destination, String toFromPrefix) {
+	private void createOD(int pop, String mode, String origion, String destination, String toFromPrefix) {
 		// Specify the number of commuters and the modal split of this relation
 		double commuters = pop * SCALEFACTOR;
-		double carcommuters = shareOfcar * commuters;
 
 		// Specify the ID of these two cities, which is the SCH attribute.
 		Geometry home = this.shapeMap.get(origion);
@@ -97,11 +77,6 @@ public class GenerateRandomDemandSolution {
 
 		// Randomly creating the home and work location of each commuters
 		for (int i = 0; i <= commuters; i++) {
-			// Specify the transportation mode
-			String mode = "car";
-			if (i > carcommuters)
-				mode = "pt";
-
 			// Specify the home location randomly and transform the coordinate
 			Coord homec = drawRandomPointFromGeometry(home);
 			homec = transformation.transform(homec);
